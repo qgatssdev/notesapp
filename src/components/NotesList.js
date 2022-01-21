c33a869788c4e6779ae60d84d85bfab719443f77
qgatssdev <chivoomodu@gmail.com>
2022-01-21 12:33:27 +0100

    creating lists

---
+++ src/components/NotesList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Note from './Note';
+
+function NotesList() {
+  return (
+    <div className="notes-list">
+      <Note />
+    </div>
+  );
+}
+
+export default NotesList;
