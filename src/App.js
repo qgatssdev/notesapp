fa5e1be133243471c361d8cc1518bebf63d2ae9a
qgatssdev <chivoomodu@gmail.com>
2022-01-26 12:09:44 +0100

    jxx

--- src/App.js
+++ src/App.js
@@ -27,6 +27,8 @@ const App = () => {
     },
   ]);
 
+  const [searchText, setSearchText] = useState('');
+
   const addNote = (text) => {
     const date = new Date();
     const newNote = {
@@ -45,9 +47,11 @@ const App = () => {
 
   return (
     <div className="container">
-      <Search />
+      <Search handleSearchNote={setSearchText} />
       <NotesList
-        notes={notes}
+        notes={notes.filter((note) =>
+          note.text.toLowerCase().includes(searchText)
+        )}
         handleAddNote={addNote}
         handleDeleteNote={deleteNote}
       />
